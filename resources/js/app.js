8d9b7a877c085b6060cba8280ea59539e2905852
khaled <ujjalbhuiyan123@gmail.com>
2024-04-26 20:34:47 +0600

    added package and segments files

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,19 @@
 import './bootstrap';
+
+import {createApp} from 'vue/dist/vue.esm-bundler.js';
+import Main from './components/IncrementCounter.vue';
+// import router from '';
+import { createPinia } from 'pinia';
+
+const app = createApp({});
+const pinia = createPinia();
+
+app.component('app-component', Main);
+
+// app.use(router);
+app.use(pinia);
+
+app.mount("#app");
+
+
+
